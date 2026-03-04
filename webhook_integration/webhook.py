33d37532d7725fa8251d08b87e85ecf9de59452e
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2026-03-04 16:23:54 +0600

    send message to facebook API

--- webhook_integration/webhook.py
+++ webhook_integration/webhook.py
@@ -4,29 +4,34 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class MessageData(BaseModel):
-    sender: str
-    recipient: str
-    platform: str
-    data: str
+    data: dict
 
 
 def process_message(data):
     print("data: ", data)
     # process the message here
+    
     # send the message to the user
     # save the message to the database
     # etc.
     # return the response
-    response = f'This is your message {data.data} and it came from {data.platform}'
+    # response = f'This is your message {data.data} and it came from {data.platform}'
     
-    return response
+    return data
 
 
 @app.post("/ai-message")
 async def webhook(data: MessageData, background_tasks: BackgroundTasks):
-    background_tasks.add_task(process_message, data)
-    print("webhook called")
-    print("response: ", data)
-    return {
-        "status": "ok", "message": "Message received"
-        }
+    try:
+        background_tasks.add_task(process_message, data)
+        print("webhook called")
+        print("response: ", data)
+        return {
+            "status": "ok", "message": "Message received"
+            }
+    except Exception as e:
+        print(f"An error occurred while processing message: {e}")
+        return {
+            "status": "error", "message": "Failed to process message"
+            }
+    
