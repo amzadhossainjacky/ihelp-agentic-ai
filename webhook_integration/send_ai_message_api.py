494dfe7b9e5835f33f3ce924e2c8b70d8402fdd3
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2026-03-05 12:32:31 +0600

    facebook connection with API and webhook

--- webhook_integration/send_ai_message_api.py
+++ webhook_integration/send_ai_message_api.py
@@ -1,9 +1,8 @@
-import requests
 import json
 
 url = "https://graph.facebook.com/v22.0/me/messages"
 
-payload = json.dumps({
+sample_payload = json.dumps({
   "messaging_type": "MESSAGE_TAG",
   "tag": "ACCOUNT_UPDATE",
   "recipient": {
@@ -13,11 +12,12 @@ payload = json.dumps({
     "text": "test facebook"
   }
 })
+
 headers = {
   'Content-Type': 'application/json',
   'Authorization': 'Bearer EAAK0ZCfTww3YBQzv90ZCcTeB4leiZBXGeGxZAAIU1KbDyh1wGv7JPfE29WAdyTq9uT2jJZBxZAQV4wVPguPlkyMLHHDSkSGYqTbZB7jWTGPJbzb2FU7qSEASbdlmogFZB1jkMNZCJLKvz8kRefDtDWhUrOi4NtZAKNaEkhZBtUd5ziA5pZCZCLf8n3tP5nEuY4zCtaKSLY6VZBt7Jq'
 }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+# response = requests.request("POST", url, headers=headers, data=payload)
 
-print(response.text)
+# print(response.text)
--- webhook_integration/webhook.py
+++ webhook_integration/webhook.py
@@ -1,5 +1,13 @@
+import json
+import os
 from fastapi import FastAPI, BackgroundTasks
 from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
+from send_ai_message_api import url, headers
+import requests
+from dotenv import load_dotenv
+load_dotenv()
 
 app = FastAPI()
 
@@ -8,14 +16,42 @@ class MessageData(BaseModel):
 
 
 def process_message(data):
-    print("data: ", data)
-    # process the message here
+    # print("data: ", data)
+    # get the message from the data body 
+    # sample data body:
+    # {'object': 'page', 'entry': [{'time': 1772614452299, 'id': '311430485396263', 'messaging': [{'sender': {'id': '8841919032488258'}, 'recipient': {'id': '311430485396263'}, 'timestamp': 1772614451570, 'message': {'mid': 'm_3GUPcAw19O8zrsNcCbiG0-3YSrgHqvT2Fq9Mw3_FPux3i3k5FiWtt0RxQ_Z6FAJKzRJ4ULRoKaqpGkglQLORMA', 'text': 'Hiii my name is pranto'}}], 'hop_context': {'app_id': 420863843067553, 'metadata': ''}}]}
+    message = data.data['entry'][0]['messaging'][0]['message']['text']
+    print("User message: ", message)
+    # get the sender id 
+    sender_id = data.data['entry'][0]['messaging'][0]['sender']['id']
+    print("Sender id: ", sender_id)
     
-    # send the message to the user
-    # save the message to the database
-    # etc.
-    # return the response
-    # response = f'This is your message {data.data} and it came from {data.platform}'
+    # process the message with ai
+    llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-4o-mini")
+    
+    ai_response = llm.invoke(
+        [SystemMessage(content="You are a helpful customer support chatbot for iHelpBD. You can help users for their queries."),
+        HumanMessage(content=message)])
+
+    # send the message to the user with API
+    payload = json.dumps({
+      "messaging_type": "MESSAGE_TAG",
+      "tag": "ACCOUNT_UPDATE",
+      "recipient": {
+            "id": sender_id
+        },
+      "message": {
+            "text": ai_response.content
+        }
+    })
+    
+    try:
+        response = requests.request("POST", url, headers=headers, data=payload)
+        print("AI message sent: ", response.text)
+    except Exception as e:
+        print(f"An error occurred while sending message: {e}")
+    finally:
+        print("AI message sent: ", response.text)
     
     return data
 
